bfce749008d16f32150ebd70093be3729ffb9989
Student <student@gu.gna>
2026-04-11 10:03:03 +0530

    added now

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    if (age=18)
+    {
+        /* code */
+    }
+    
+    cout<<"hello world!" <<endl;
+    return 0; 
+    
+}
+                                                                    
